87cbc1b0b9c100ad6ecf1b20a320a9d755c2398f
Alfredo Alonso Anchante Moreno <alonso.amoreno@live.com>
2024-06-05 12:54:08 -0500

    Trabajando en el Auth Layout

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,28 +1,56 @@
 import HomePage from '@/modules/landing/pages/HomePage.vue'
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHashHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomePage
+      name: 'landing',
+      component: () => import('@/modules/landing/layouts/LandingLayout.vue'),
+      children: [
+        {
+          path: '/',
+          name: 'home',
+          component: HomePage
+        },
+        {
+          path: 'features',
+          name: 'features',
+          component: () => import('@/modules/landing/pages/FeaturesPage.vue')
+        },
+        {
+          path: 'pricing',
+          name: 'pricing',
+          component: () => import('@/modules/landing/pages/PricingPage.vue')
+        },
+        {
+          path: 'contact',
+          name: 'contact',
+          component: () => import('@/modules/landing/pages/ContactPage.vue')
+        }
+      ]
     },
+    // Auth
     {
-      path: '/features',
-      name: 'features',
-      component: () => import('@/modules/landing/pages/FeaturesPage.vue')
-    },
-    {
-      path: '/pricing',
-      name: 'pricing',
-      component: () => import('@/modules/landing/pages/PricingPage.vue')
-    },
-    {
-      path: '/contact',
-      name: 'contact',
-      component: () => import('@/modules/landing/pages/ContactPage.vue')
+      path: '/auth',
+      name: 'auth',
+      redirect(to) {
+        return { name: 'login' }
+      },
+      component: () => import('@/modules/auth/layouts/AuthLayout.vue'),
+      children: [
+        {
+          path: 'login',
+          name: 'login',
+          component: () => import('@/modules/auth/pages/LoginPage.vue')
+        },
+        {
+          path: 'register',
+          name: 'register',
+          component: () => import('@/modules/auth/pages/RegisterPage.vue')
+        }
+      ]
     }
   ]
 })
